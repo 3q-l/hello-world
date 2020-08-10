138a3adaa1c21c2d30d7baed7b7d49c42a22c9ad
1170940385@qq.com <asd1230.0>
2020-08-10 22:07:44 +0800

    a

--- src/main/java/com/lsp/helloworld/algorithm/sort/MergeSort.java
+++ src/main/java/com/lsp/helloworld/algorithm/sort/MergeSort.java
@@ -1,36 +1,45 @@
 package com.lsp.helloworld.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * 归并排序 O(n log2^n)
  * Created by lisp on 2020/7/10/下午4:28.
  */
 public class MergeSort {
     public static void main(String[] args) {
-        int[] data = {1,3,6,4,5};
-        sort(data);
+        int[] data = {1,8,7,4,10,6,9};
+        Arrays.sort(data);
+        sort(data,0,data.length - 1);
         SortUtils.print(data);
     }
 
-    public static void sort(int[] data){
-//        merge(data);
+    public static void sort(int[] data,int left,Integer right){
+        if (left < 0 || right <= 0 || left >= right){
+            return;
+        }
+        // 分成两半
+        int mid = left + (right - left) / 2;
+        // 左边排序
+        sort(data,left,mid);
+        // 右边排序
+        sort(data,mid + 1,right);
+        merge(data,left,mid + 1,right);
     }
 
     public static void merge(int[] data,int leftPtr,int rightPtr,int rightBound){
-        int length = data.length;
-        int mid = length / 2;
+        int mid = rightPtr - 1;
         int[] tamp = new int[rightBound - leftPtr + 1];
         int i = leftPtr;
         int j = rightPtr;
         int k = 0;
-        while (i <= mid && j < length){
-            if (data[i] <= data[j]){
-                tamp[k++] = data[i++];
-            } else {
-                tamp[k++] = data[j++];
-            }
+        while (i <= mid && j <= rightBound){
+            tamp[k++] = data[i] <= data[j] ? data[i++] : data[j++];
         }
         while (i <= mid) {tamp[k++] = data[i++];}
-        while (j < length) {tamp[k++] = data[j++];}
-        SortUtils.print(tamp);
+        while (j <= rightBound) {tamp[k++] = data[j++];}
+        for (int m = 0; m < tamp.length; m++) {
+            data[leftPtr + m] = tamp[m];
+        }
     }
 }
